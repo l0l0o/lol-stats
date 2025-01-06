eee39166f6cc14bfe771ba5f7b9be78c4d313602
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2025-01-06 14:14:45 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { HighchartsChartModule } from 'highcharts-angular';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HighchartsChartModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lol-stats';
+
+  // Highcharts properties
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    title: {
+      text: 'Highcharts Example in Standalone Component',
+    },
+    xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    },
+    yAxis: {
+      title: {
+        text: 'Values',
+      },
+    },
+    series: [
+      {
+        type: 'line',
+        name: 'Sample Data',
+        data: [10, 20, 30, 40, 50, 60],
+      },
+    ],
+  };
 }
